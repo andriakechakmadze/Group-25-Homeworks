4d615e00fc57e81c2374be82318895dbe27b6b52
andriakechakmadze <andriakechakmadze789@gmail.com>
2024-09-05 15:37:22 +0400

    'commit'

---
+++ level 29/homework/level 29.py
@@ -0,0 +1,7 @@
+
+def even_index():
+    list = [2,4,7,5,8]
+    print(list[0])
+    print(list[2])
+    print(list[4])
+even_index()
---
+++ level 31/homework/level 31.py
@@ -0,0 +1,7 @@
+#დავალება: შექმენით ფუნქცია, რომელიც მომხმარებელს შეკითხება პაროლს, სანამ სოწორს არ ჩაწერს მომხმარებელი.
+password=82486
+number = int (input("enter number"))
+while number != password:
+
+    number = int (input("enter number"))
+    
